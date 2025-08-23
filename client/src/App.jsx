52995856f802fe8b7ed3ef4c864a075004bddf01
shana202 <shana@bignoyes.com>
2025-08-23 14:18:18 -0400

    updated auth and added favorites

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './pages/home'
-import City from './pages/city'
-// import Login from './pages/login'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './auth/AuthProvider';
+import Home from './pages/home';
+import City from './pages/city';
+import Login from './pages/login';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/city/:slug" element={<City />} />
-        {/* <Route path="/login/:slug?" element={<Login />} />
-        <Route path="*" element={<div>Page not found</div>} /> */}
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/city/:slug" element={<City />} />
+          <Route path="/login/" element={<Login />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   )
 };
